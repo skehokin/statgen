9470ae66248bf9d4948585a484486aab9d685bf7
Siobhan Hokin <skehokin@gmail.com>
2019-05-17 06:57:15 +1000

    Updates

---
+++ precache-manifest.529099909e16debf794c9387d5879632.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "46660924cf16828f9ad4c389d895ec8d",
+    "url": "/statgen/index.html"
+  },
+  {
+    "revision": "05462e004b12c02a1ef5",
+    "url": "/statgen/static/css/main.91cb2e1a.chunk.css"
+  },
+  {
+    "revision": "72acc8b974d9378b0900",
+    "url": "/statgen/static/js/2.0e492812.chunk.js"
+  },
+  {
+    "revision": "05462e004b12c02a1ef5",
+    "url": "/statgen/static/js/main.33569015.chunk.js"
+  },
+  {
+    "revision": "e1c003d7de05414fc575",
+    "url": "/statgen/static/js/runtime~main.1eed20e1.js"
+  },
+  {
+    "revision": "13b95043019589f4d823dc4faf77af66",
+    "url": "/statgen/static/media/denny-muller-532466-unsplash5.13b95043.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/statgen/static/media/logo.5d5d9eef.svg"
+  }
+]);
